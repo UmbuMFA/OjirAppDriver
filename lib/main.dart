5ee60365cc9323f17bfa67d0e891e143c2eeedcd
fadhlis <39572912+fadhlis@users.noreply.github.com>
2022-07-24 07:27:59 +0700

    splashscreen

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,16 @@ import 'package:driver_app/AllScreens/loginScreen.dart';
 import 'package:driver_app/AllScreens/mainscreen.dart';
 import 'package:driver_app/AllScreens/registerationScreen.dart';
 import 'package:driver_app/DataHandler/appData.dart';
+import 'package:splashscreen/splashscreen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
-  runApp(MyApp());
+  runApp(MaterialApp(
+    home: MyApp(),
+    debugShowCheckedModeBanner: false,
+  ));
 }
 
 DatabaseReference bankSampahRef = FirebaseDatabase.instance.ref("bank_sampahs");
@@ -28,8 +32,28 @@ DatabaseReference rideRequestRef = FirebaseDatabase.instance
     .child(currentfirebaseUser.uid)
     .child("newRide");
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return SplashScreen(
+        seconds: 2,
+        navigateAfterSeconds: AfterSplash(),
+        image: Image.asset('images/logo.png'),
+        backgroundColor: Colors.white,
+        styleTextUnderTheLoader: const TextStyle(),
+        photoSize: 100.0,
+        onClick: () => print("Flutter Egypt"),
+        loaderColor: Colors.green);
+  }
+}
+
+class AfterSplash extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
